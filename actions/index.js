459bfb807a11364f7335576da7888901a73c8789
fut0k0 <cccabell@icloud.com>
2020-05-05 17:22:35 -0700

    Passing lab

--- actions/index.js
+++ actions/index.js
@@ -1,32 +1,69 @@
-'use strict';
+"use strict"
 
 function addColumn (ev) {
+	const prevTable = [...this.state.table];
+	const table = prevTable.map(row => row.concat(""));
+
+	ev.preventDefault();
+	this.setState({table});
 }
 
 function addRow (ev) {
+	const prevTable = [...this.state.table];
+	const newRow = prevTable[0].map(col => "");
+	const table = prevTable.concat([newRow]);
+
+	ev.preventDefault();
+	this.setState({table});
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+	const table = [...this.state.table]
+
+	table[rowIndex][columnIndex] = ev.target.value;
+	this.setState({table});
 }
 
 function focusCell (rowIndex, columnIndex) {
+	this.setState({
+		focused: [rowIndex, columnIndex]
+	})
 }
 
 function blurCell () {
+	this.setState({
+		focused: null
+	})
 }
 
 function removeRow (ev) {
+	const prevTable = [...this.state.table]
+
+	ev.preventDefault();
+	if (prevTable.length > 1) {
+		const table = prevTable.slice(0, -1)
+
+		this.setState({table})
+	};
 }
 
 function removeColumn (ev) {
+	const prevTable = [...this.state.table]
+
+	ev.preventDefault();
+	if (prevTable[0].length > 1) {
+		const table = prevTable.map(row => row.slice(0, -1))
+
+		this.setState({table})
+	};
 }
 
 module.exports = {
-  addColumn,
-  addRow,
-  changeCell,
-  focusCell,
-  blurCell,
-  removeRow,
-  removeColumn
-};
+	addColumn,
+	addRow,
+	changeCell,
+	focusCell,
+	blurCell,
+	removeRow,
+	removeColumn
+}
--- components/Row.js
+++ components/Row.js
@@ -1,11 +1,23 @@
-'use strict';
+"use strict"
 
-const React = require('react');
-const Cell = require('./Cell');
+const React = require("react");
+const Cell = require("./Cell");
 
 class Row extends React.Component {
-  render () {
-  }
+	render () {
+		const {row, onChange, onFocus} = this.props
+
+		return (
+			<tr className="row">
+				{row.map((cell, i) =>
+					<Cell key={i}
+								cell={cell}
+								onChange={onChange.bind(null, i)}
+								onFocus={onFocus.bind(null, i)} />
+				)}
+			</tr>
+		)
+	}
 }
 
-module.exports = Row;
+module.exports = Row
